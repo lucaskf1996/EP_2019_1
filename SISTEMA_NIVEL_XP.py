fb73e82e2d51af7b943d824783ab9c096c80ad4b
lucaskf1996 <lucaskf1996@gmail.com>
2019-04-15 17:11:23 -0300

    Sistema de nível simplificado.

---
+++ SISTEMA_NIVEL_XP.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 16:46:16 2019
+
+@author: fuque
+"""
+##### O QUE PRECISA FAZER? #####
+'''
+CADA MONSTRO DA 1 DE XP
+'''
+
+def nivel_e_xp(nivel_jogador, xp_jogador, cap_xp):
+    if xp_jogador+1==cap_xp:
+        nivel_jogador+=1
+        cap_xp*=2
+        xp_jogador=0
+    else:
+        xp_jogador+=1
+    return [nivel_jogador, xp_jogador, cap_xp]
+
+nivel_do_jogador=1
+cap_xp_jogador=1
+xp_do_jogador=0
