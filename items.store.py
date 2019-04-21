39dfe7534d2167735c00db495be1da78b5c321ae
jpvarella <49522220+jpvarella@users.noreply.github.com>
2019-04-21 13:46:00 -0300

    Create items.store.py

---
+++ items.store.py
@@ -0,0 +1,23 @@
+def buy_item(itens_na_mochila,dinheiro):
+    loja = {
+  'protótipo quebrado' : 10
+, 'pochete': 15
+, 'lata de coca': 15
+, 'espada de papelão': 20
+}
+    print (loja)
+    print (dinheiro)
+    buy = input('Qual item quer comprar? (escreva o nome certo...) Digite "sair" se quiser fechar a loja')
+    while buy != 'sair':
+        if buy in loja:
+            if dinheiro >= loja[buy]:
+                itens_na_mochila.append[buy]
+                dinheiro -= loja[buy]
+                del(loja[buy])
+                print(loja)
+                print(dinheiro)
+                buy = input('Qual item quer comprar? (escreva o nome certo...) Digite "sair" se quiser fechar a loja')
+        else:
+            print ('Esse item não existe... (escreveu certo?)')
+            buy = input('Qual item quer comprar? (escreva o nome certo...)')
+    return (itens_na_mochila,dinheiro)
