e0c8e428bb648b4e250208d997d846b21a3f8b12
lucaskf1996 <49496013+lucaskf1996@users.noreply.github.com>
2019-04-21 15:42:36 -0300

    Update items_store.py
    ajustei formatação de texto, sintaxe do append e condição do while

--- items_store.py
+++ items_store.py
@@ -5,17 +5,19 @@ def buy_item(itens_na_mochila,dinheiro):
 , 'lata de coca': 15
 , 'espada de papelão': 20
 }
-    print (loja)
-    print (dinheiro)
+    for i in loja:
+        print("-", i," ", loja[i])
+    print ("/n Voce tem", dinheiro, "dinheiros")
     buy = input('Qual item quer comprar? (escreva o nome certo...) Digite "sair" se quiser fechar a loja')
-    while buy != 'sair':
+    while buy != 'sair' or loja == {}:
         if buy in loja:
             if dinheiro >= loja[buy]:
-                itens_na_mochila.append[buy]
+                itens_na_mochila.append(buy)
                 dinheiro -= loja[buy]
                 del(loja[buy])
-                print(loja)
-                print(dinheiro)
+                for i in loja:
+                    print("-", i," ", loja[i])
+                print ("/n Voce tem", dinheiro, "dinheiros")
                 buy = input('Qual item quer comprar? (escreva o nome certo...) Digite "sair" se quiser fechar a loja')
             else:
                 print ('Você não tem dinheiro pra comprar esse item... (é a crise)')
