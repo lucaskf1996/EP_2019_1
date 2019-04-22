d802dfbf47e0b31eabdc5af1c07c7ea9025a23f0
Amanda Carmo <amanda.rosa.ismart@gmail.com>
2019-04-22 20:11:23 -0300

    sala secreta

---
+++ techlab.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 22 19:05:14 2019
+
+@author: User
+"""
+
+import random
+
+def techlab (mochila):
+    mochila.append("foda-se")
+    print("\nvocê recebeu o item 'foda-se'\n")
+    return mochila
